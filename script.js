e06129eea7dbcf98e2a59b7b97960d79cf58b509
kanchanstha <39837341+kanchanstha@users.noreply.github.com>
2025-05-10 01:29:42 +0000

    Restored to 'be7a53e92de0328583d18078ce8c097ebdecb292'
    Replit-Restored-To: be7a53e92de0328583d18078ce8c097ebdecb292

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new FinisherHeader({
-        "count": 90,
-        "size": {
-            "min": 1,
-            "max": 20,
-            "pulse": 0
-        },
-        "speed": {
-            "x": {
-                "min": 0,
-                "max": 0.4
-            },
-            "y": {
-                "min": 0,
-                "max": 0.1
-            }
-        },
-        "colors": {
-            "background": "#2558a2",
-            "particles": [
-                "#ffffff",
-                "#87ddfe",
-                "#acaaff",
-                "#1bffc2",
-                "#f88aff"
-            ]
-        },
-        "blending": "screen",
-        "opacity": {
-            "center": 0,
-            "edge": 0.4
-        },
-        "skew": -2,
-        "shapes": [
-            "c",
-            "s",
-            "t"
-        ]
-    });
 
     const header = document.querySelector('.site-header');
     const navLinks = document.querySelectorAll('.main-nav a[href^="#"]');
